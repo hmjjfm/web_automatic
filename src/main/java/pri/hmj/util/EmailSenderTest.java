fe7a3dfef0a7a37e39fd3f24a784b6aa343dcf60
hmjjfm <823246224@qq.com>
2018-08-14 21:37:03 +0800

    4

--- src/main/java/pri/hmj/util/EmailSenderTest.java
+++ src/main/java/pri/hmj/util/EmailSenderTest.java
@@ -5,18 +5,14 @@ import java.io.File;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.testng.annotations.Test;
 
-@ContextConfiguration(classes = { RootConfig.class })
-@WebAppConfiguration
 public class EmailSenderTest {
 
-	@Autowired
 	private JavaMailSender mailSender;
 
 	@Test
@@ -26,17 +22,18 @@ public class EmailSenderTest {
 		mailSender = config.mailSender();
 		MimeMessage message = mailSender.createMimeMessage();
 		MimeMessageHelper helper = new MimeMessageHelper(message, true);
-		/*
-		 * message.setFrom("15759675251@163.com");//发件人
-		 * message.setTo("easyeye@163.com");//收件人
-		 */ 
+		
+		 // message.setFrom("15759675251@163.com");//发件人
+		 // message.setTo("easyeye@163.com");//收件人
+		  message.setSubject("sss");
+		 
 		helper.setFrom("15759675251@163.com");
-		helper.setTo("easyeye@163.com");
-		helper.setSubject("Spring Email Test");// 主题
-		helper.setText("hello world!!");// 正文
-		File file1 =new File(System.getProperty("user.dir")+"\\target\\surefire-reports\\index.html");  
+		helper.setTo("huanfuan@163.com");
+		helper.setSubject("我在用发送邮件");// 主题
+		helper.setText("成功了吗!");// 正文
+		File file1 =new File(System.getProperty("user.dir")+"\\log\\log.log");  
 		//注意文件名
-		helper.addAttachment("index.html", file1);
+		helper.addAttachment("log.log", file1);
 		
 		mailSender.send(message);
 		System.out.println("邮件发送完毕");
